ac650025f69a60ae9092244c221856a7dec57659
Muxtarzade <smuxtarzade2004@gmail.com>
2023-06-10 00:48:21 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="topBar">
+        <div className="container">
+          <div className="row">
+            <div className="leftSideBar">
+                <ul>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <section className="home">
+        <h1>Home</h1>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
