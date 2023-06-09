66627a202fb74d5684264f1bab5751c71bda1578
Muxtarzade <smuxtarzade2004@gmail.com>
2023-06-10 01:41:13 +0400

    Top Bar css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <div className="topBar">
         <div className="container">
           <div className="row">
